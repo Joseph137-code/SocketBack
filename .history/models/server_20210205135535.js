1df8a70817b80c88c9c506095bf560dee481933b
Jose Sierra <segjsierra@gmail.com>
2021-02-05 13:58:23 -0300

    base

--- .history/models/server_20210205135535.js
+++ .history/models/server_20210205135535.js
@@ -14,7 +14,7 @@ class Server {
     constructor() {
 
         this.app  = express();
-        this.port = process.env.PORT || 8080;
+        this.port = process.env.port || 8080;
 
         // Conectar a DB
         dbConnection();
