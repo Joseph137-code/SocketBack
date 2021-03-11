af4b867ad8509b0aecfe3891dbec74cdc5960a90
Jose Sierra <segjsierra@gmail.com>
2021-03-11 13:38:14 -0300

    cors

--- models/server.js
+++ models/server.js
@@ -23,7 +23,7 @@ class Server {
         this.server = http.createServer( this.app );
         
         // Configuraciones de sockets
-        this.io = socketio(this.server, { cors: { methods: ["GET", "POST"], }, }); }
+        this.io = socketio( this.server, { cors: { methods: ["GET", "POST"], }} );
     }
 
     middlewares() {
